d0383ebbc0ca3054cfcfe62c85d6e4bd21a03dc7
Joseph Ikehara <jikehara@carroll.edu>
2018-01-19 12:50:53 -0700

    added Application controller test

--- test/ApplicationTest.java
+++ test/ApplicationTest.java
@@ -36,10 +36,18 @@ public class ApplicationTest {
 
     @Test
     public void renderTemplate() {
-        Content html = views.html.index.render("Hello, World");
+        Content html = views.html.index.render("hello, world");
         assertThat(contentType(html)).isEqualTo("text/html");
-        assertThat(contentAsString(html)).contains("Hello, World");
+        assertThat(contentAsString(html)).contains("hello, world");
     }
 
-
+	@Test
+    public void testIndex() {
+        Result result = controllers.Application.index();
+        assertThat(status(result)).isEqualTo(OK);
+        assertThat(contentType(result)).isEqualTo("text/html");
+        assertThat(charset(result)).isEqualTo("utf-8");
+        assertThat(contentAsString(result)).contains("hello, world");
+    }
+	
 }
