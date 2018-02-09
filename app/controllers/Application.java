fd493fb04abe7c0cae3cc8c10bbb547298048181
Joseph Ikehara <jikehara@carroll.edu>
2018-02-09 12:41:56 -0700

    Adding stuff from Nate's Hibernate lab

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -11,20 +11,20 @@ public class Application extends Controller {
         return ok(index.render("hello, world", play.data.Form.form(models.Task.class)));
     }
 
+	@play.db.jpa.Transactional
 	public static Result addTask() {
         play.data.Form<models.Task> form = play.data.Form.form(models.Task.class).bindFromRequest();
         if (form.hasErrors()) {
             return badRequest(index.render("hello, world", form));
-        }
-        else {
+        } else {
             models.Task task = form.get();
-            task.save();
+            play.db.jpa.JPA.em().persist(task);
             return redirect(routes.Application.index());
         }
     }
     
     public static Result getTasks() {
-        java.util.List<models.Task> tasks = new play.db.ebean.Model.Finder(String.class, models.Task.class).all();
+        java.util.List<models.Task> tasks = new java.util.ArrayList<>();
         return ok(play.libs.Json.toJson(tasks));
     }    
     
--- app/models/Task.java
+++ app/models/Task.java
@@ -1,17 +1,30 @@
 package models;
 
-import play.db.ebean.Model;
-
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity
-public class Task extends Model {
+public class Task {
     
     @Id
-    public String id;
+    private String id;
     
     @play.data.validation.Constraints.Required
-    public String contents;
+    private String contents;
     
+    public String getId() {
+    	return id;
+    }
+    
+    public void setId(String id) {
+    	this.id = id;
+    }
+    
+    public String getContents() {
+    	return contents;
+    }
+    
+    public void setContents(String contents) {
+    	this.contents = contents;
+    }
 }
