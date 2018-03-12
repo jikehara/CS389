44a41532626881246c7c880106ac54d40fb545a8
cajones <cajones@carroll.edu>
2018-03-12 13:20:18 -0600

    Working Spring config w/out service

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,8 +2,7 @@ package controllers;
 
 import models.Task;
 import play.data.Form;
-import play.db.jpa.JPA;
-import play.db.jpa.Transactional;
+
 import play.mvc.Controller;
 import play.mvc.Result;
 
@@ -13,18 +12,27 @@ import org.slf4j.LoggerFactory;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
 import views.html.index;
 
+@Named
 public class Application extends Controller {
 
 	private static final Logger logger = LoggerFactory.getLogger(Application.class);
 	
-	public static Result index() {
+	@PersistenceContext
+	private EntityManager manage;
+	
+	public Result index() {
         return ok(index.render("hello, world", Form.form(models.Task.class)));
     }
 
 	@Transactional
-	public static Result addTask() {
+	public Result addTask() {
 		logger.info("Adding a task...");
         play.data.Form<models.Task> form = play.data.Form.form(models.Task.class).bindFromRequest();
         if (form.hasErrors()) {
@@ -32,15 +40,15 @@ public class Application extends Controller {
             return badRequest(index.render("hello, world", form));
         } else {
             models.Task task = form.get();
-            JPA.em().persist(task);
+            manage.persist(task);
             logger.info("Added a task!");
             return redirect(routes.Application.index());            
         }
     }
     
-	@Transactional
-    public static Result getTasks() {
-		List<Task> tasks = JPA.em().createQuery("FROM Task", Task.class).getResultList();
+	
+    public Result getTasks() {
+		List<Task> tasks = manage.createQuery("FROM Task", Task.class).getResultList();
         return ok(play.libs.Json.toJson(tasks));
     }    
     
--- build.sbt
+++ build.sbt
@@ -7,12 +7,20 @@ lazy val root = (project in file(".")).enablePlugins(PlayJava)
 scalaVersion := "2.11.6"
 
 libraryDependencies ++= Seq(
-  javaJdbc,
   cache,
-  javaWs,
+  javaJdbc,
   javaJpa.exclude("org.hibernate.javax.persistence", "hibernate-jpa-2.0-api"),
   "org.hibernate" % "hibernate-entitymanager" % "4.3.8.Final",
-  "mysql" % "mysql-connector-java" % "5.1.40"
+  "mysql" % "mysql-connector-java" % "5.1.40",
+  "javax.inject" % "javax.inject" % "1",
+  "org.springframework" % "spring-aop"             % "4.1.6.RELEASE",
+  "org.springframework" % "spring-beans"           % "4.1.6.RELEASE",
+  "org.springframework" % "spring-context"         % "4.1.6.RELEASE",
+  "org.springframework" % "spring-core"            % "4.1.6.RELEASE",
+  "org.springframework" % "spring-jdbc"            % "4.1.6.RELEASE",
+  "org.springframework" % "spring-orm"             % "4.1.6.RELEASE",
+  "org.springframework" % "spring-tx"              % "4.1.6.RELEASE",
+  "org.springframework" % "spring-test"            % "4.1.6.RELEASE" % "test"
 )
 
 libraryDependencies += "org.webjars" % "jquery" % "1.11.2"
