b1374854eca73124f4ef5efbf24116b2aaa7336e
Joseph Ikehara <ikehajos@gmail.com>
2018-04-22 21:18:14 -0600

    High Scores should now be visible on /home due to changes in coffeescript.

--- app/services/UserService.java
+++ app/services/UserService.java
@@ -4,9 +4,24 @@ import models.User;
 import models.UserInfo;
 
 public interface UserService {
+	/**
+	 * Checks a user to see if it is null or matches the username of a different user
+	 * @param user
+	 * @return false if the user is null or matches an existing user, otherwise true
+	 */
     boolean addUser(User user);
 
+    /**
+     * 
+     * @param username
+     * @return
+     */
     boolean userExists(String username);
 
+    /**
+     * 
+     * @param username
+     * @return
+     */
     UserInfo getUserData(String username);
 }
