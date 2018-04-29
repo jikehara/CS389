f6f1dd4387aa0c6ecb19500297dd459c3f9ff800
Joseph Ikehara <ikehajos@gmail.com>
2018-04-29 17:16:05 -0600

    Added some more tests for score validation.

--- test/services/ScorePersistenceServiceTest.java
+++ test/services/ScorePersistenceServiceTest.java
@@ -22,17 +22,50 @@ public class ScorePersistenceServiceTest extends AbstractTransactionalJUnit4Spri
 	
 	@Inject
 	private ScoreService scoreService;
-	@Inject
-	private UserService userService;
 
-	// user add tests
+	// user/score add tests
+	
 	// test being able to add a user/score
 	@Test
 	public void addScoreTest() {
 		ScoreForm s = new ScoreForm();
 		UserForm u = new UserForm();
+		u.setUsername("user0");
 		s.setScore((long) 15);
 		assertTrue(scoreService.addScore(u, s));
 	}
 	
+	// cannot add a score that is lower than a previous score
+	@Test
+	public void addLowerScore() {
+		ScoreForm s = new ScoreForm();
+		UserForm u = new UserForm();
+		u.setUsername("user0");
+		s.setScore((long) 15); // score of 15
+		assertTrue(scoreService.addScore(u, s));
+		s.setScore((long) 10); // score of 10
+		assertFalse(scoreService.addScore(u, s));
+	}
+	
+	// can add a score that is the new highest score
+	@Test
+	public void addHigherScore() {
+		ScoreForm s = new ScoreForm();
+		UserForm u = new UserForm();
+		u.setUsername("user0");
+		s.setScore((long) 15); // score of 15
+		assertTrue(scoreService.addScore(u, s));
+		s.setScore((long) 100); // score of 100
+		assertTrue(scoreService.addScore(u, s));
+	}
+	
+	// cannot add an initial negative score
+	@Test
+	public void addNegativeScore() {
+		ScoreForm s = new ScoreForm();
+		UserForm u = new UserForm();
+		u.setUsername("user0");
+		s.setScore((long) -1);
+		assertFalse(scoreService.addScore(u, s));
+	}
 }
